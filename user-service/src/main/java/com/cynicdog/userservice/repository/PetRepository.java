893cfdb0d9b98f84680b3c96e7dfb9d9025bb1a2
ginsenglee <thecynicdog0328@gmail.com>
2023-11-04 14:18:20 +0900

    repository

---
+++ user-service/src/main/java/com/cynicdog/userservice/repository/PetRepository.java
@@ -0,0 +1,9 @@
+package com.cynicdog.userservice.repository;
+
+import com.cynicdog.userservice.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
--- user-service/src/main/java/com/cynicdog/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/cynicdog/userservice/repository/UserRepository.java
@@ -2,7 +2,13 @@ package com.cynicdog.userservice.repository;
 
 import com.cynicdog.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByFirstNameAndMiddleNameAndLastName(String FirstName, String MiddleName, String LastName);
 
 }
